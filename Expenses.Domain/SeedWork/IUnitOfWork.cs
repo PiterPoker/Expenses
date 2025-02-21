1134986218b4b134f77a2e40a581c3d97f6be2af
PiterPoker <tema5286@gmail.com>
2025-02-22 01:34:15 +0300

    feat: Added base classes Entity, UnitOfWork and ValueObject

---
+++ Expenses.Domain/SeedWork/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Expenses.Domain.SeedWork;
+public interface IUnitOfWork : IDisposable
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default);
+}
