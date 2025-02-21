ca5fe58ace95d573c54db85beb5b70cfd9b99cf7
PiterPoker <tema5286@gmail.com>
2025-02-22 01:35:16 +0300

    feat: Добавлены сущности Author, Category, Expense, Money и Wallet

---
+++ Expenses.Domain/Entities/Wallet.cs
@@ -0,0 +1,46 @@
+﻿using Expenses.Domain.SeedWork;
+
+namespace Expenses.Domain.Entities;
+
+/// <summary>
+/// Represents a wallet.
+/// </summary>
+public class Wallet : Entity
+{
+    /// <summary>
+    /// Gets or sets the name of the wallet. This property is required.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Parameterless constructor required by EF Core.  Should not be used directly.
+    /// </summary>
+    protected Wallet() { }
+
+    /// <summary>
+    /// Creates a new Wallet.
+    /// </summary>
+    /// <param name="name">The name of the wallet.</param>
+    public Wallet(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentNullException(nameof(name), "Wallet name cannot be null or whitespace.");
+        }
+
+        Name = name;
+    }
+
+    /// <summary>
+    /// Updates the name of the wallet.
+    /// </summary>
+    /// <param name="name">The new name of the wallet.</param>
+    public void UpdateName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentNullException(nameof(name), "Wallet name cannot be null or whitespace.");
+        }
+        Name = name;
+    }
+}
