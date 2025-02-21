9d4940d720eb3faf244dc56c18c71b98fb79797e
PiterPoker <tema5286@gmail.com>
2025-02-22 01:36:29 +0300

    feat: Add interfaces for factories, repositories, and services

---
+++ Expenses.Domain/Interfaces/Repositories/IExpenseRepository.cs
@@ -0,0 +1,31 @@
+﻿using Expenses.Domain.Entities;
+
+namespace Expenses.Domain.Interfaces.Repositories;
+
+/// <summary>
+/// Интерфейс для репозитория расходов.
+/// </summary>
+public interface IExpenseRepository : IRepository<Expense>
+{
+    /// <summary>
+    /// Получает список расходов за определенный период времени.
+    /// </summary>
+    /// <param name="startDate">Начальная дата периода.</param>
+    /// <param name="endDate">Конечная дата периода.</param>
+    /// <returns>Список расходов.</returns>
+    Task<List<Expense>> GetExpensesByPeriodAsync(DateTime startDate, DateTime endDate);
+
+    /// <summary>
+    /// Получает список расходов по определенной категории.
+    /// </summary>
+    /// <param name="categoryId">Идентификатор категории.</param>
+    /// <returns>Список расходов.</returns>
+    Task<List<Expense>> GetExpensesByCategoryIdAsync(int categoryId);
+
+    /// <summary>
+    /// Получает список расходов по определенному кошельку.
+    /// </summary>
+    /// <param name="walletId">Идентификатор кошелька.</param>
+    /// <returns>Список расходов.</returns>
+    Task<List<Expense>> GetExpensesByWalletIdAsync(int walletId);
+}
